d4093718ad298583a94af8cb871b2e537388aa22
Likesh Singla <Singlalikesh81@gmail.com>
2025-09-22 22:03:52 +0530

    Questions

--- Problem-61.c
+++ Problem-61.c
@@ -22,11 +22,13 @@ Output 2:
 
 int main() {
     int n, i, key;
+    printf("Enter number of elements: ");
     scanf("%d", &n);
     int arr[n];
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    printf("Enter element to search: ");
     scanf("%d", &key);
     for (i = 0; i < n; i++) {
         if (arr[i] == key) {
--- Problem-63.c
+++ Problem-63.c
@@ -15,11 +15,13 @@ Output 1:
 
 int main() {
     int n1, n2, i;
+    printf("Enter number of elements in first array: ");
     scanf("%d", &n1);
     int arr1[n1];
     for (i = 0; i < n1; i++) {
         scanf("%d", &arr1[i]);
     }
+    printf("Enter number of elements in second array: ");
     scanf("%d", &n2);
     int arr2[n2];
     for (i = 0; i < n2; i++) {
